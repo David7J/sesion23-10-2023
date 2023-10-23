055ab433cb0c97a50d5b472b055660c0602ac5e2
David Jeshua Áreas Pérez <142626297+David7J@users.noreply.github.com>
2023-10-23 17:27:11 -0600

    Add files via upload

---
+++ Ejercicios/App.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int Arreglo[] = {3,5,8,9,6,2};
+        //Pedir al usuario que ingrese los valores enteros a ordenar
+
+        System.out.println("Arreglo original: ");
+        mostrarArreglo(Arreglo, Arreglo.length);
+
+        OrdenarPorInsercion(Arreglo, Arreglo.length);
+        System.out.println("Arreglo ordenado: ");
+        mostrarArreglo(Arreglo, Arreglo.length);
+    }
+
+    public static void OrdenarPorInsercion(int Arr[], int n){
+        for (int  i = 0; i < Arr.length; i++) {
+            int valorC = Arr[i];
+            int posicion = i;
+            //Verificar si se esta trabajando con el elemento que esta despues del primero
+            //y si el elemento que esta de primero es mayor que el segundo
+            while((posicion > 0) && (Arr[posicion - 1] > valorC)){
+                Arr[posicion] = Arr[posicion - 1]; //Cambiamos de posicion
+                posicion = posicion - 1;
+            }
+            Arr[posicion] = valorC;
+
+        }
+    }
+
+    public static void mostrarArreglo(int Arr[], int n){
+        for (int i = 0; i < Arr.length; i++) {
+            System.out.print(Arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+
+}
